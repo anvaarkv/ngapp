66169637f203c18b196cafca5fb3054ba6a7cc83
Anvaar Baig <46243347+anvaarkv@users.noreply.github.com>
2019-03-19 17:32:15 +0500

    Initial commit

---
+++ src/app/courses/courses.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.css']
+})
+export class CoursesComponent implements OnInit {
+  newcomponent = "Entered in new component created";
+  months = ["January", "Feburary", "March", "April", "May", 
+  "June", "July", "August", "September",
+  "October", "November", "December"];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
