cfcafe1f5cfc8b0fb0a9a155b8a79377dfa0cf64
Anvaar Baig <46243347+anvaarkv@users.noreply.github.com>
2019-04-03 19:43:59 +0500

    updated

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -10,9 +10,16 @@ export class CoursesComponent implements OnInit {
   months = ["January", "Feburary", "March", "April", "May", 
   "June", "July", "August", "September",
   "October", "November", "December"];
+  date: string = new Date().toDateString();
+  time: string = new Date().toLocaleTimeString();
+  datetime: string;
   constructor() {
-
-   }
+    setInterval(()=> {
+      let currentdate = new Date();
+      this.datetime = currentdate.toDateString() + " " + currentdate.toLocaleTimeString();
+    
+    },1000);
+    }
 
   ngOnInit() {
   }
